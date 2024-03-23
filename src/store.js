f6e18daf1d63d989b03d8896bc7031f494af718b
mina-atallah <minaatallah18@gmail.com>
2024-03-23 08:45:27 +0200

    Created new State for Customer - renamed the previous reducer to accountReducer - created new reducer for the state of the customer

--- src/store.js
+++ src/store.js
@@ -1,21 +1,29 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
 /*
   - PURE CODE (REDUX WITHOUT REACT), to understand REDUX in isolation.
   - then we integrate the two together!
   - SECTION GOAL: is to model the bank account to be a bit simpler without an account number and onlu with a balance , ..
 */
-const initialState = {
+const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
+
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+// REDUCERS
 /* 
   -the goal of reducer is to calculate the state based on the current state and the received action
   -reducers aren't allowed to modify the existing state and not allowed to do any asynchronous logic or other side effects
   -one big difference between this reducer and the reducer in useReducer hook: usually we directly pass initialState as the default value
 */
-function reducer(state = initialState, action) {
+function accountReducer(state = initialStateAccount, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
@@ -42,16 +50,38 @@ function reducer(state = initialState, action) {
   }
 }
 
+function customerReducer(state = initialStateCustomer, action) {
+  switch (action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state,
+        fullName: action.payload.fullName,
+        nationalID: action.payload.nationalID,
+        createdAt: action.payload.createdAt,
+      };
+    case "customer/updateName":
+      return {
+        ...state,
+        fullName: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+// COMINED REDUCER/ ROOT RODUCER
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
 // REDUX
-const store = createStore(reducer);
-// store.dispatch({ type: "account/deposit", payload: 500 });
-store.dispatch(deposit(300));
-store.dispatch(withdraw(120));
-store.dispatch(requestLoan(5000, "Buying a laptop"));
-console.log(store.getState());
-store.dispatch(payLoan());
+const store = createStore(rootReducer);
+store.dispatch(updateName("M.M.Shendi"));
+store.dispatch(deposit(500));
 console.log(store.getState());
 
+// ACTIONS
 function deposit(amount) {
   return { type: "account/deposit", payload: amount };
 }
@@ -66,4 +96,15 @@ function requestLoan(amount, purpose) {
 }
 function payLoan() {
   return { type: "account/payLoan" };
+}
+
+function createCustomer(fullName, nationalID) {
+  return {
+    type: "customer/createCustomer",
+    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
+  };
+}
+
+function updateName(fullName) {
+  return { type: "customer/updateName", payload: fullName };
 }
