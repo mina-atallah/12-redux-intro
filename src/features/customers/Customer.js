e2995da108e7036e49ab6c9aa445b1fc2f42cb81
mina-atallah <minaatallah18@gmail.com>
2024-03-24 07:36:41 +0200

    Connect Redux with App & Dispatch Actions - installed react-redux to read data from store and disptach actions to it to update the state - used useSelector hook to read the the data - used useDisptach hook to disptach the actions to the store reducers

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,5 +1,12 @@
+/* 
+  - creates a subscribtion to the store (reads data from the store)
+*/
+import { useSelector } from "react-redux";
+
 function Customer() {
-  return <h2>👋 Welcome, %NAME%</h2>;
+  const customer = useSelector((store) => store.customer.fullName);
+
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
