8af00c6171243118d4d6c7fac21992bc7ac2d528
mina-atallah <minaatallah18@gmail.com>
2024-03-29 12:42:51 +0200

    Refactor State Slices - used createSlice in customerSlice and accountSlice to generate the action creators and actions types that correspond to the reducer and state

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,12 +1,74 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    requestLoan: {
+      // because action creator function receices onlu one argument
+      prepare(amount, purpose) {
+        return {
+          payload: {
+            amount,
+            purpose,
+          },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        state.loan = action.payload.amount;
+        state.balance = state.balance + action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+      },
+    },
+    payLoan(state, action) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    convertingCurrency(state) {
+      state.isLoading = true;
+    },
+  },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    // API CALL
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    const convertedCurrency = data.rates.USD;
+    // Dispatch Action
+    dispatch({ type: "account/deposit", payload: convertedCurrency });
+  };
+}
+
+export default accountSlice.reducer;
+
+/*
 // Reducer
-export default function accountReducer(state = initialStateAccount, action) {
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -70,3 +132,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+*/
---
+++ src/store-v2.js
@@ -0,0 +1,31 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+// middleware (to manage asynchronous operations and side effects within your Redux-based applications.)
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+/*
+  - SECTION GOAL: is to model the bank account to be a bit simpler without an account number and onlu with a balance , ..
+*/
+
+// REDUCERS
+/* 
+  -the goal of reducer is to calculate the state based on the current state and the received action
+  -reducers aren't allowed to modify the existing state and not allowed to do any asynchronous logic or other side effects
+  -one big difference between this reducer and the reducer in useReducer hook: usually we directly pass initialState as the default value
+*/
+
+// REDUX
+// COMINED REDUCER/ ROOT RODUCER
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
